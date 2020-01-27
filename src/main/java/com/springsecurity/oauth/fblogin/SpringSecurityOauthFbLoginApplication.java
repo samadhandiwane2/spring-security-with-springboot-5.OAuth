1af7ee6a69974d23e0f976c67b87428e829d0b6f
f9661zh <f9661zh@VP-1U2FINSYX047.1DC.com>
2020-01-27 19:14:19 +0530

    First Commit

---
+++ src/main/java/com/springsecurity/oauth/fblogin/SpringSecurityOauthFbLoginApplication.java
@@ -0,0 +1,15 @@
+package com.springsecurity.oauth.fblogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class SpringSecurityOauthFbLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthFbLoginApplication.class, args);
+	}
+
+}
